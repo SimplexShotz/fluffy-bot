0c9e266621af13a38828b15af71419653806129c
SimplexShotz <brancy280@gmail.com>
2020-05-12 16:01:20 -0400

    trying to get public IP

--- bot.js
+++ bot.js
@@ -21,8 +21,15 @@ var ref = {
   users: database.ref("users")
 };
 
-var ip = require('ip');
-console.log(ip.address());
+const publicIp = require('public-ip');
+
+(async () => {
+    console.log(await publicIp.v4());
+    //=> '46.5.21.123'
+
+    console.log(await publicIp.v6());
+    //=> 'fe80::200:f8ff:fe21:67cf'
+})();
 
 // Setup Request:
 const request = require("request");
